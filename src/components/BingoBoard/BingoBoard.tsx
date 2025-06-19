8699881b134a2cbf4c48a4acf1dda30c027fdbce
Samuel Stenfelt <stenfeltsamuel@gmail.com>
2025-06-19 22:40:17 +0200

    modal

--- src/components/BingoBoard/BingoBoard.tsx
+++ src/components/BingoBoard/BingoBoard.tsx
@@ -1,9 +1,25 @@
 import './BingoBoard.css'
 import BingoCard from '../BingoCard/BingoCard'
+import { useEffect, useState } from 'react';
+import Modal from 'react-modal'
 
 function BingoBoard({data}: any) {
+    const [isOpen, setIsOpen] = useState(true);
+
+    function openModal() {
+        setIsOpen(true)
+    }
+
+    function closeModal() {
+        setIsOpen(false)
+    }
+
     return (
         <div className='bingo-board'>
+            <Modal isOpen={isOpen}>
+                <button onClick={() => setIsOpen(false)}>Stäng</button>
+                <p>Hello!</p>
+            </Modal>
             {data.map((information: any) =>
                 <BingoCard key={information.id} information={information} />
             )}
